6b6fa8cfe0d3746653413b8548fb02ac97fac78e
1HongyeXiong <hongye.xiong@outlook.com>
2023-10-03 11:57:59 -0700

    the first page

---
+++ client/src/App.js
@@ -0,0 +1,63 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href = "" className = "logo" >MyBlog</a>
+        <nav>
+          <a href = "">Login</a>
+          <a href = "">Register</a>
+        </nav>
+      </header>
+     
+      <div className = "post">
+        <div className = "image">
+          <img src = "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1hDLyc.img?w=534&h=401&m=6&x=436&y=174&s=139&d=139"></img>
+        </div>
+        <div className = "texts">
+          <h2>McCarthy speaker vote live updates</h2>
+          <p className = "info">
+            <a className = "author">Hongye Xiong</a>
+            <time>2023-01-06 16:45</time>
+          </p>
+          <p className = "summary">House Minority Leader Hakeem Jeffries speaks to reporters at the U.S. Capitol in Washington, D.C., Sept. 30, 2023.</p>
+        </div>
+      </div>
+
+      <div className = "post">
+        <div className = "image">
+          <img src = "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1hDLyc.img?w=534&h=401&m=6&x=436&y=174&s=139&d=139"></img>
+        </div>
+        <div className = "texts">
+          <h2>McCarthy speaker vote live updates</h2>
+          <p className = "info">
+            <a className = "author">Hongye Xiong</a>
+            <time>2023-01-06 16:45</time>
+          </p>
+          <p className = "summary">House Minority Leader Hakeem Jeffries speaks to reporters at the U.S. Capitol in Washington, D.C., Sept. 30, 2023.</p>
+        </div>
+      </div>
+
+      <div className = "post">
+        <div className = "image">
+          <img src = "https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1hDLyc.img?w=534&h=401&m=6&x=436&y=174&s=139&d=139"></img>
+        </div>
+        <div className = "texts">
+          <h2>McCarthy speaker vote live updates</h2>
+          <p className = "info">
+            <a className = "author">Hongye Xiong</a>
+            <time>2023-01-06 16:45</time>
+          </p>
+          <p className = "summary">House Minority Leader Hakeem Jeffries speaks to reporters at the U.S. Capitol in Washington, D.C., Sept. 30, 2023.</p>
+        </div>
+      </div>
+
+
+
+    </main>
+  );
+}
+
+export default App;
